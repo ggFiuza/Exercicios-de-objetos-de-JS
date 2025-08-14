acc5ae006be1b2c3dbaff9811d8fbe421bd44807
ggFiuza <guilherme.fiuza@estudante.iftm.edu.br>
2025-08-14 08:47:28 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,28 +1,66 @@
-function criarFilme() {
-    let titulo = prompt("Digite o título do filme:")
-    let genero = prompt("Digite o gênero do filme:")
-    let nota = parseFloat(prompt("Digite a nota do filme:"))
-    
-    return {
-        titulo: titulo,
-        genero: genero,
-        nota: nota
-    }
+let primeirofilme ={
+
+    nome: " ",
+    genero: " ",
+    nota: " "
 }
 
-let filme1 = criarFilme()
-let filme2 = criarFilme()
+let segundofilme ={
 
-console.log("Dados do primeiro filme:")
-console.log(filme1)
-console.log("Dados do segundo filme:")
-console.log(filme2)
+    segundonome:" ",
+    segundogenero: " ",
+    segundanota: " "
+}
 
-if (filme1.nota > filme2.nota) {
-    console.log("Filme com melhor nota: " + filme1.titulo)
-} else if (filme2.nota > filme1.nota) {
-    console.log("Filme com melhor nota: " + filme2.titulo)
-} else {
-    console.log("Ambos os filmes têm a mesma nota!")
+let nome = prompt("Qual o nome do primeiro filme?: ")
+nome = String(nome)
+primeirofilme.nome = nome
+
+let genero = prompt("Qual o gênero do primeiro filme?: ")
+genero = String(genero)
+primeirofilme.genero = genero
+
+let nota = prompt("Qual sua nota para o filme?: ")
+nota = Number(nota)
+primeirofilme.nota = nota
+
+
+
+
+
+
+let segundonome = prompt("Digite o nome do segundo filme: ")
+segundofilme = String(segundonome)
+segundofilme.segundonome = segundonome
+
+let segundogenero = prompt("Qual o gênero do segundo filme?: ")
+segundogenero = String(segundogenero)
+primeirofilme.segundogenero = segundogenero
+
+let segundanota = prompt("Digite o valor da sua nota para ele: ")
+segundanota = Number(segundanota)
+segundofilme.segundanota = segundanota
+
+
+
+
+
+
+console.log("Nome do primeiro filme: ", primeirofilme.nome)
+console.log("Genêro do primeiro filme: ", primeirofilme.genero)
+console.log("A nota do primeiro filme: ", + primeirofilme.nota)
+
+
+console.log("Nome do segundo filme: ", segundofilme.segundonome)
+console.log("Genêro do segundo filme: ", segundofilme.segundogenero)
+console.log("A nota do segundo filme: ", + segundofilme.segundanota)
+
+
+if(segundofilme.segundanota > primeirofilme.nota){
+
+    console.log("O filme com a maior nota é: ", segundofilme.segundonome)
+} else{
+
+    console.log("O filme com a maior nota é: ", primeirofilme.nome)
 }
 
